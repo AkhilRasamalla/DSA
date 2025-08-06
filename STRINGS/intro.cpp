689c9989ee95cd60ab58f5898334c90d15ba1a25
AkhilRasamalla <akhilrasamalla0805@gmail.com>
2025-08-06 12:17:04 +0530

    INTRODUCTION TO STRINGS

---
+++ STRINGS/intro.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char name[20];
+    cout<<"enter your name: ";
+    cout<<endl;
+    cin>>name;
+
+    //accessing the string 
+    name[0] = '\0';
+    
+    cout<<"your name is  " ;
+    cout<<name<<endl;
+
+
+
+
+
+}
