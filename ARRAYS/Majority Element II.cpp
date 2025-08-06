45115d1dfaaa914c4b7c9fd9510c53e228fa8741
AkhilRasamalla <akhilrasamalla0805@gmail.com>
2025-08-06 11:40:28 +0530

    solved one leetcode question it is similarly to majority element but slight modification

---
+++ ARRAYS/Majority Element II.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int cnt1 = 0;
+        int ele1;
+        int cnt2 = 0;
+        int ele2;
+
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && nums[i] != ele2) {
+                cnt1 = 1;
+                ele1 = nums[i];
+            } else if (cnt2 == 0 && nums[i] != ele1) {
+                cnt2 = 1;
+                ele2 = nums[i];
+
+            } else if (ele1 == nums[i])
+                cnt1++;
+            else if (ele2 == nums[i])
+                cnt2++;
+
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        // manual check;
+        vector<int> ls;
+        cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (ele1 == nums[i])
+                cnt1++;
+            if (ele2 == nums[i])
+                cnt2++;
+        }
+        int mini = (int)(n / 3) + 1;
+        if (cnt1 >= mini)
+            ls.push_back(ele1);
+        if (cnt2 >= mini && ele2 != ele1)
+            ls.push_back(ele2);
+        sort(ls.begin(), ls.end());
+
+        return ls;
+    }
+};
