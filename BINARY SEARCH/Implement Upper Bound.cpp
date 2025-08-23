eafb7dbd99712ff7cad6624079162570c242220c
AkhilRasamalla <akhilrasamalla0805@gmail.com>
2025-08-24 00:01:31 +0530

    solved upper bound problem using the binary search algorithm in GFG

---
+++ BINARY SEARCH/Implement Upper Bound.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        // code here
+        if(arr.size()==0)
+        return 0;
+        int low =0;
+        int high = arr.size()-1;
+        int ans = arr.size();
+        while(low <= high){
+          int  mid = low + (high-low)/2;
+            if(arr[mid] > target){
+                ans = mid ;
+                high = mid-1;
+
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+        
+        
+        
+        
+        
+    }
+};
