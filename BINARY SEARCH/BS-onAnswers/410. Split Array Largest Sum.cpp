f122719d3de5cc7c50903b82492cfe92f2757411
AkhilRasamalla <akhilrasamalla0805@gmail.com>
2025-09-06 16:30:22 +0530

    this problem is exactly similar to the another problem painters partion both are exactly similar  both cna be solved by the previous we solved Allocation of boooks

---
+++ BINARY SEARCH/BS-onAnswers/410. Split Array Largest Sum.cpp
@@ -0,0 +1,44 @@
+class Solution {
+private:
+    int CountStudents(vector<int>& arr, int pages) {
+        int students = 1;
+        long long pagesStudent = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (pagesStudent + arr[i] <= pages) {
+                pagesStudent += arr[i];
+
+            } else {
+                students += 1;
+                pagesStudent = arr[i];
+            }
+        }
+        return students;
+    }
+
+public:
+    int findPages(vector<int>& arr, int k) {
+        // code here
+        // edge case
+        if (k > arr.size())
+            return -1;
+
+        long long low = *max_element(arr.begin(), arr.end());
+        long long high = accumulate(arr.begin(), arr.end(), 0LL);
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+            int students = CountStudents(arr, mid);
+            if (students > k) {
+                low = mid + 1;
+
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+public:
+    int splitArray(vector<int>& nums, int k) {
+        return findPages(nums,k);
+    }
+};
