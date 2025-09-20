44e7f100259d2e7e18c3c2315e632c002a47ca96
AkhilRasamalla <akhilrasamalla0805@gmail.com>
2025-09-20 15:46:11 +0530

    solved the question using the greedy approach

---
+++ GREEDY ALGORITHMS/Minimum Platforms.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int minPlatform(vector<int>& arr, vector<int>& dep) {
+        // code here
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        sort(dep.begin(),dep.end());
+        int plat_needed = 1 , result =1 ;
+        int i = 1, j = 0;
+        while(i < n && j<n){
+            if(arr[i] <= dep[j]){
+                plat_needed++;
+                i++;
+            }
+            else{
+                plat_needed--;
+                j++;
+            }
+            if(plat_needed > result){
+                result = plat_needed;
+            }
+        }
+        return result;
+    }
+};
