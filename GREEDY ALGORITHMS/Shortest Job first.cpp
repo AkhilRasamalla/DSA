e6a25106132d3f021be02bed4c613767c97bda7f
AkhilRasamalla <akhilrasamalla0805@gmail.com>
2025-09-22 23:24:20 +0530

    solved the problem Shortest job first

---
+++ GREEDY ALGORITHMS/Shortest Job first.cpp
@@ -0,0 +1,23 @@
+// User function Template for C++
+
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+    long long solve(vector<int>& bt) {
+        // code here
+        sort(bt.begin(),bt.end());
+        
+        long long n = bt.size();
+        long long total_waiting_time  = 0;
+        long long curr_time  = 0;
+        
+        for(int i=0;i<n;i++){
+            total_waiting_time  += curr_time;
+            curr_time += bt[i];
+        }
+        
+        return total_waiting_time / n;
+        
+    }
+};
